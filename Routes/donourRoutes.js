ff3ab8d390e9987738c518bc8d8d77ad091164cf
Biswajit Dey <bdey11333@gmail.com>
2024-06-03 23:47:21 +0530

    Auth middleware contains name and email

--- Routes/donourRoutes.js
+++ Routes/donourRoutes.js
@@ -4,10 +4,11 @@ const addDonourConroller = require('../Controllers/addDonour')
 const otpValidateController = require('../Controllers/otpValidate')
 const viewDonourController = require('../Controllers/viewDonour')
 const logincontroller = require('../Controllers/loginControl')
+const authControl = require('../Middlewares/authController')
 
 router.post("/forms", addDonourConroller)
 router.post("/register", otpValidateController)
 router.post("/login", logincontroller)
-router.get("/view", viewDonourController)
+router.get("/view",authControl, viewDonourController)
 
 module.exports = router
